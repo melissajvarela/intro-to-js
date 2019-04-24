6862b389c69b7495534ec4dd8689ebfa722f6fc9
unknown <mjanayvarela@gmail.com>
2019-04-23 19:13:22 -0700

    try to push

---
+++ js/number.js
@@ -0,0 +1,53 @@
+//number data type
+
+var a = 10; 
+console.log(a);
+
+
+var b = 100;
+console.log(b);
+
+var a = 20;
+console.log (a);
+
+var c = 0x1E;
+console.log(c);
+
+var d = 0.5;
+console.log(d);
+
+
+//addition
+var sum = a + b;
+console.log(sum);
+
+
+
+//subtraction
+var diff = d-c;
+console.log(diff);
+
+
+//multiplication
+var mult = a * d;
+console.log(mult);
+
+//divison
+
+var div = a / d;
+console.log(div);
+
+var div = d / a;
+console.log(div);
+
+//modulus 
+ var mod = b % a;
+ console.log(mod);
+
+ var mod = b % c;
+ console.log(mod);
+
+
+ //not a number;but is a string
+ var e = '5';
+ console.log (typeof e); //returns the type of variable e
---
+++ js/string.js
@@ -0,0 +1,9 @@
+//string data type
+
+var str1 = 'it\'s Tuesday Today!';
+console.log(str1);
+
+var str2 = "We are learning Javascript Today!";
+console.log(str2);
+
+console.log(str1, str2);
