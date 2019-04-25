d6ae910402c80137b6749faa93a57a16a7840487
unknown <mjanayvarela@gmail.com>
2019-04-24 20:28:18 -0700

    added review data types js file in js folder and added 5 integer variales

---
+++ js/review-data-types-1.js
@@ -0,0 +1,26 @@
+// a. create 5 integer variables
+//b. create 5 string variables
+// c. create 2 boolean variables
+// d. create 5 array variables
+// e. create 5 object variables
+// f. output/display all these variables in console 
+
+// beginning of 5 integer variables
+var a = 1;
+console.log(a);
+
+var b = 2;
+console.log(b);
+
+var sum = a + b;
+console.log(sum);
+
+var diff = a - b;
+console.log(diff);
+
+var mult = a * b;
+console.log(mult);
+
+//end of 5 integer variables
+
+//beginning of 5 string variables
