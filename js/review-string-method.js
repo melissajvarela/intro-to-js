db8ec21ccccd4e8c6b7ecab13afdce5940f99ba0
unknown <mjanayvarela@gmail.com>
2019-04-25 10:23:01 -0700

    added carmodel string

--- js/review-string-method.js
+++ js/review-string-method.js
@@ -1,10 +1,22 @@
-var firstName = "Hello";
+// Beginning of car model string
 
-var last_name="World!";
+var str1 = "Hello";
 
-var fullName = firstName + ' ' + last_name;
+var str2="World!";
+
+var fullName = str1 + ' ' +str2;
 console.log(fullName);
-console.log(firstName);
-console.log(last_name);
+console.log(str1);
+console.log(str2);
+// end of car model string
 
+//beginning of car model string
+var str3="honda";
 
+var str4="civic";
+
+console.log(str3, str4);
+
+console.log(str3);
+console.log(str4);
+// end of car model string
