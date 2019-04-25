45a15ff0e7a79ac03ba95f29147facb1a9261b6d
unknown <mjanayvarela@gmail.com>
2019-04-25 14:20:22 -0700

    added first and last name object

---
+++ js/review-object.js
@@ -0,0 +1,8 @@
+var person = {
+    firstName: 'Melissa',
+    lastName: 'Varela'
+  };
+console.log(person.firstName,person.lastName);
+
+var fullName = person.firstName.concat(' ', person.lastName);
+ console.log(fullName);
