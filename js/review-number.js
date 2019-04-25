07a0701dbafc8c90800a00778b13e5dda1d41f05
unknown <mjanayvarela@gmail.com>
2019-04-24 23:06:39 -0700

    added review number js file and linked it to html and added output math equations

---
+++ js/review-number.js
@@ -0,0 +1,29 @@
+var a = 65;
+var b = 24;
+
+
+// addition
+var sum = a + b;
+console.log(sum);
+
+//subtraction
+var diff = a-b;
+console.log(diff);
+
+
+//multiplication
+var mult = a * b;
+console.log(mult);
+
+//divison
+
+var div = a / b;
+console.log(div);
+
+var div = a / b;
+console.log(div);
+
+//modulus 
+ var mod = b % a;
+ console.log(mod);
+
